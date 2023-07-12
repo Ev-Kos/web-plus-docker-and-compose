7d567052780c1a676859ff537b1eb7a2bdf4d302
Evgeniya Kostyrya <zhenya241191@gmail.com>
2023-07-12 16:17:00 +0300

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  const corsOptions = {
+    origin: [
+      'http://kpd-frontend.nomoredomains.work',
+      'https://kpd-frontend.nomoredomains.work',
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: ['authorization', 'content-type'],
+  };
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(4000);
 }
